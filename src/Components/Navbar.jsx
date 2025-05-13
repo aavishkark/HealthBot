a0f5e382eb1498e3c89be24e4f1e08202944dc6d
Avishkar Kamble <aavishkark@gmail.com>
2025-05-13 17:30:12 +0530

    Add a navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,24 @@
+import './navbar.css';
+import { useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
+import { LOGOUT } from '../Redux/Login/actionType';
+import { useNavigate } from 'react-router-dom';
 export const Navbar = () => {
+    const isAuth = useSelector((store) => store.AuthReducer.isAuth);
+    const dispatch = useDispatch();
+     const navigate = useNavigate();
+    const handleLogout = () => {
+        dispatch({ type: LOGOUT });
+        localStorage.setItem('isAuth', false);
+        localStorage.removeItem('token');
+        navigate('/login');
+      }
     return (
-        <div>
+        <div className='navbar'>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/profile">Profile</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
+                {isAuth === "true" ? <li onClick={handleLogout}><a href="/login">Logout</a></li> : <li><a href="/login">Login</a></li>}
             </ul>
         </div>
     )
