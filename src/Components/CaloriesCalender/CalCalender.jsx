7ca8ddb9f89691be9276651155cd4c6b29784de7
Avishkar Kamble <aavishkark@gmail.com>
2025-05-31 23:00:30 +0530

    Add table to modal

--- src/Components/CaloriesCalender/CalCalender.jsx
+++ src/Components/CaloriesCalender/CalCalender.jsx
@@ -1,10 +1,33 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
+import { useDisclosure, Button, ChakraProvider} from '@chakra-ui/react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Box,
+} from '@chakra-ui/react';
 import './calcalender.css';
+import {Modal} from '@chakra-ui/react' ;
+import { Bar } from 'react-chartjs-2';
+import {
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
 
-function CalorieCalendar({ calories, onDateClick, requiredcalories }) {
+function CalorieCalendar({ calories, onDateClick, requiredcalories, selectedDayEntries }) {
   const [value, setValue] = useState(new Date());
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const groupedCalories = calories.reduce((acc, entry) => {
     const date = new Date(entry.timestamp).toDateString();
@@ -49,7 +72,17 @@ function CalorieCalendar({ calories, onDateClick, requiredcalories }) {
   const handleDateChange = (date) => {
     setValue(date);
     if (onDateClick) onDateClick(date);
+    onOpen()
   };
+
+  const optionsDate = {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    };
 
   return (
     <div className='calContainer'>
@@ -57,7 +90,48 @@ function CalorieCalendar({ calories, onDateClick, requiredcalories }) {
         onChange={handleDateChange}
         value={value}
         tileContent={tileContent}
+        w="100%"
       />
+      <ChakraProvider>
+        <Modal isOpen={isOpen} onClose={onClose} size="5xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <Box overflowX="auto">
+                  <Table variant="striped" colorScheme="teal" size="md">
+                    <Thead>
+                      <Tr>
+                        <Th>Food Item</Th>
+                        <Th>Amount(g)</Th>
+                        <Th isNumeric>Calories</Th>
+                        <Th isNumeric>Proteins(g)</Th>
+                        <Th isNumeric>Fats(g)</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {selectedDayEntries.map((item, idx) => (
+                        <Tr key={idx}>
+                          <Td>{item.foodItem}</Td>
+                          <Td>{item.foodAmount}</Td>
+                          <Td isNumeric>{item.calories}</Td>
+                          <Td isNumeric>{item.proteins}</Td>
+                          <Td isNumeric>{item.fats}</Td>
+                        </Tr>
+                      ))}
+                    </Tbody>
+                  </Table>
+                </Box>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </ChakraProvider>
     </div>
   );
 }
